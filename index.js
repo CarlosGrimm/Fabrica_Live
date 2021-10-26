3f6f5c956dd25eb73d3c7b635f9e2a5688902522
Carlos Grimm <grimmjunior@gmail.com>
2021-10-26 11:02:31 -0400

    Update index.js
    Listagem de endpoints e criação dos endpoints de leitura(individual e todos)

--- index.js
+++ index.js
@@ -6,5 +6,49 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
- 
+
+/*
+  Lista de Endpoints da aplicação CRUD de mensagens
+  CRUD: Create, Read (Single & All), Update and Delete
+  CRUD: Criar, Ler (Individual e Tudo), Atualizar e Remover
+    - [GET] /mensagens - Retorna a lista de mensagens
+    - [GET] /mensagens/{id} - Retorna apenas uma única mensagem pelo ID
+    - [POST] /mensagens - Cria uma nova mensagem
+    - [PUT] /mensagens/{id} - Atualiza uma mensagem pelo ID
+    - [DELETE] /mensagens/{id} - Remover uma mensagem pelo ID
+*/
+
+const mensagens = [
+                    "Primeira Mensagem",
+                    "Segunda Mensagem"
+                  ];
+
+//- [GET] /mensagens - Retorna a lista de mensagens
+app.get('/mensagens', (req, res) => {
+  res.send(mensagens);
+});
+
+//- [GET] /mensagens/{id} - Retorna apenas uma única mensagem pelo ID
+app.get('/mensagens/:id', (req, res) => {
+  const id = req.params.id - 1;
+  const mensagem = mensagens[id];
+  res.send(mensagem);
+});
+
+
+//- [POST] /mensagens - Cria uma nova mensagem
+app.get('/mensagens', (req, res) => {
+  res.send(mensagens)
+});
+
+//- [PUT] /mensagens/{id} - Atualiza uma mensagem pelo ID
+app.get('/mensagens', (req, res) => {
+  res.send(mensagens)
+});
+
+//- [DELETE] /mensagens/{id} - Remover uma mensagem pelo ID                  
+app.get('/mensagens', (req, res) => {
+  res.send(mensagens)
+});
+
 app.listen(port);

bfebb5a194c231becde2ef2826ffdd6afab1c112
Carlos Grimm <grimmjunior@gmail.com>
2021-10-26 12:33:36 -0400

    Criação das funções para pegar mensagens pelo ID e validações adicionadas e atualização no POSTMAN

--- index.js
+++ index.js
@@ -25,9 +25,18 @@ app.get('/', (req, res) => {
 */
 
 const mensagens = [
-                    "Primeira Mensagem",
-                    "Segunda Mensagem"
-                  ];
+                    {
+                      "id": 1,
+                      "texto": "Primeira Mensagem",
+                    },
+                    {
+                      "id": 2,
+                      "texto": "Segunda Mensagem",
+                    },
+                  ]
+
+
+const getMensagemById = id => mensagens.find(msg => msg.id === id);
 
 //- [GET] /mensagens - Retorna a lista de mensagens
 app.get('/mensagens', (req, res) => {
@@ -36,33 +45,48 @@ app.get('/mensagens', (req, res) => {
 
 //- [GET] /mensagens/{id} - Retorna apenas uma única mensagem pelo ID
 app.get('/mensagens/:id', (req, res) => {
-  const id = req.params.id - 1;
-  const mensagem = mensagens[id];
-  res.send(mensagem);
+  const id = +req.params.id;
+  const mensagem = getMensagemById(id);
+
+  if(!mensagem) {
+    res.send("Mensagem não Encontrada.");  
+    return;
+  }else{
+    res.send(mensagem);
+  }
+  
 });
 
 
 //- [POST] /mensagens - Cria uma nova mensagem
 app.post('/mensagens', (req, res) => {
-  const mensagem = req.body.mensagem;
-  mensagens.push[mensagem];
-  res.send(`Mensagem criada com sucesso: '${mensagem}'.`);
+  const mensagem = req.body;
+  mensagem.id = mensagens.length+1;
+  mensagens.push(mensagem);
+  res.send(mensagem);
 });
 
 //- [PUT] /mensagens/{id} - Atualiza uma mensagem pelo ID
 app.put('/mensagens/:id', (req, res) => {
-  const id = req.params.id;
-  const mensagem = req.body.mensagem;
-  mensagens[id] = mensagem;
-  res.send(`Mensagem alterada com sucesso: '${mensagem}'.`);
+  const id = +req.params.id;
+  const mensagem = getMensagemById(id);
+
+  const novoTexto = req.body.texto;
+  mensagem.texto = novoTexto;
+  
+  res.send(mensagem);
 });
 
 //- [DELETE] /mensagens/{id} - Remover uma mensagem pelo ID                  
 app.delete('/mensagens/:id', (req, res) => {
-  const id = req.params.id - 1;
-  const mensagem = mensagens[id];
-  mensagens.splice(id, 1);
-  res.send(`Mensagem removida com sucesso: '${mensagem}'.`);
+  const id = +req.params.id;
+  const mensagem = getMensagemById(id);
+
+  const index = mensagens.indexOf(mensagem);
+
+  mensagens.splice(index,1);
+
+  res.send("Mensagem removida com sucesso.");
 });
 
 app.listen(port);
